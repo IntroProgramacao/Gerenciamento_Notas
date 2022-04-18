4cb52f16f14ee1846cb875dc7b0f8475a6ce8c3d
IntroProgramacao <lucas.goncalves.dev@gmail.com>
2022-04-17 22:14:27 -0300

    Funções atualizadas para abertura de arquivos

--- gerenciador.c
+++ gerenciador.c
@@ -16,38 +16,36 @@ Autor: Lucasitron
 #define MAX_MAT 25
 #define MAX_NAM 50
 
-
+//verifica se um arquivo foi aberto corretamente
+void VerAber(FILE *abre){
+    if (abre == NULL)
+    {
+        perror("FALHA DE ABERTURA: o arquivo não foi aberto corretamente!");
+    }
+}
 
 int
 main(int argc, char *argv[])
 {
 	 //recebe o nome de uma turma para armazenar os nomes dos alunos
-     char Nturma[15];
+     string Nturma;
      Nturma = get_string("Digite o nome da turma");
 
      FILE *turma;
      turma = fopen(Nturma, "a");
      
-     //quando a variavel do tipo FILE  contém valor nulo houve um erro de abertura
-     if (materias == NULL)
-     {
-         printf("ERRO!\nFalha de abertura");
-         return 1;
-     }
+     //verifica se o arquivo foi aberto corretamente
+     VerAber(turma);
 
      //Recebe uma matéria para armazenar as notas
-     char Materia[MAX_MAT];
+     string Materia;
      Materia = get_string("Digite a componente: \n");
 
      //Abre um arquivo para armazenar as notas dos alunos por matéria
      FILE *materias;
      materias = fopen(Materia, "a");
-     //quando a variavel do tipo FILE  contém valor nulo houve um erro de abertura
-     if (materias == NULL)
-     {
-         printf("ERRO!\nFalha de abertura");
-         return 2;
-     }
-
+     
+     // verifica a abertura do arquivo
+     VerAber(materias);
           
 }
